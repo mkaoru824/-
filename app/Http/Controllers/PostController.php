04971bf45ff5f2cbb634730059522e2d2e54f915
mkaoru824 <runlun03@gmail.com>
2024-01-15 22:37:14 +0900

    kakunin

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts' => $post->get()]);  
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+    }
+}
+?>
---
+++ app/Models/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Datebase\Eloquent\Factories\HasFactory;
+use Illuminate\Datebase\Eloquent\model;
+
+class Post extends Model
+{
+    use HasFactory;
+}
