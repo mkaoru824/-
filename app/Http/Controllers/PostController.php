e55e6b4d1bc0350201d63ae45984543e31fb2e30
mkaoru824 <runlun03@gmail.com>
2024-01-18 10:36:41 +0900

    kadai2made

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class PostController extends Controller
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Datebase\Eloquent\Factories\HasFactory;
-use Illuminate\Datebase\Eloquent\model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
