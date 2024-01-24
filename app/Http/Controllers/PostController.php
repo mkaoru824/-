075b31e6b15478fcbcf58fed4cb26903e1475ac1
mkaoru824 <runlun03@gmail.com>
2024-01-24 17:13:21 +0900

    kadai3.4made

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,8 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->get()]);  
-       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+       //getPaginateByLimit()はPost.phpで定義したメソッドです。
     }
 }
 ?>
